10c03a7d9857eafa690f7de6135865b4abe299e9
meirzhan <meirzhan.anarbekuly@gmail.com>
2026-04-09 00:30:32 +0500

    commit -m "@EntityGraph(attributePaths = {"images"})"

--- src/main/java/pro/routes/repository/RouteRepository.java
+++ src/main/java/pro/routes/repository/RouteRepository.java
@@ -1,10 +1,15 @@
 package pro.routes.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pro.routes.model.Route;
 
+import java.util.List;
+
 @Repository
 public interface RouteRepository extends JpaRepository<Route, Long> {
     // Здесь уже есть методы findAll, save, delete и т.д.
+    @EntityGraph(attributePaths = {"images"})
+    List<Route> findAll();
 }
